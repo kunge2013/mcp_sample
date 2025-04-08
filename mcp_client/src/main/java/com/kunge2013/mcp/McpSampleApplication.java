208cc9922323547b2aba0c567fd62516134ed675
kunge2013 <543976974@QQ.com>
2025-04-08 13:10:47 +0800

    init

---
+++ mcp_client/src/main/java/com/kunge2013/mcp/McpSampleApplication.java
@@ -0,0 +1,13 @@
+package com.kunge2013.mcp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McpSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McpSampleApplication.class, args);
+	}
+
+}
